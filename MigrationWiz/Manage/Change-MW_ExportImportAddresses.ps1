13b3c1d514e0b983bd540a59c015d7f33264e3ef
Antonio Vargas <34351859+amvargas76@users.noreply.github.com>
2020-07-31 10:39:17 -0400

    Update Change-MW_ExportImportAddresses.ps1
    synopsys update

--- MigrationWiz/Manage/Change-MW_ExportImportAddresses.ps1
+++ MigrationWiz/Manage/Change-MW_ExportImportAddresses.ps1
@@ -1,14 +1,17 @@
 <#
-
-.SYNOPSIS
-
 Copyright 2020 BitTitan, Inc.
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. 
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 
 You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, 
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+#>
+
+<#
+
+.SYNOPSIS
+    This script changes source and destination address, in a MigrationWiz project, in bulk.
 
 .DESCRIPTION
     This script will export the migration line items under the selected project or for all projects to a CSV file for you to review.  
